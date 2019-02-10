d0932bb5cd1c29ce50a9de4172b8d8dedbb47c67
Xiangyu Li <47231543+XiangyuLiCCA@users.noreply.github.com>
2019-02-09 20:02:23 -0800

    Update HM 3 HSB demo.js

--- HM 3 HSB demo.js
+++ HM 3 HSB demo.js
@@ -2,13 +2,13 @@ function setup() {
     createCanvas(400, 400);
     background(220);
     // HSB with H range 0-400, S range 0-400, B range 0-100:
-    colorMode(HSB, 400, 400, 100);
+    colorMode(300,59,95,243,100,243);
 }
 
 function draw() {
     let x = random(width);
     let y = random(height);
-    fill(x, y, 100);
+    fill(x, y, 120);
     noStroke()
-    ellipse(x, y, 20);
+    ellipse(x, y, 30);
 }
